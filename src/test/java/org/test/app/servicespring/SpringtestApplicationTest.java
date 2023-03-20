daf273c415d115d9604ff145c864c0f3643e8d53
fjuanias <faiberjuanias001@gmail.com>
2023-03-20 17:08:44 -0500

    Change service test

---
+++ src/test/java/org/test/app/servicespring/SpringtestApplicationTest.java
@@ -0,0 +1,6 @@
+package org.test.app.servicespring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestApplicationTest {}
---
+++ src/test/java/org/test/app/servicespring/services/CuentaServiceImplTest.java
@@ -0,0 +1,102 @@
+package org.test.app.servicespring.services;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.test.app.servicespring.exceptions.DineroInsuficienteException;
+import org.test.app.servicespring.irepositories.IBancoRepository;
+import org.test.app.servicespring.irepositories.ICuentaRepository;
+import org.test.app.servicespring.iservices.ICuentaService;
+import org.test.app.servicespring.models.Banco;
+import org.test.app.servicespring.models.Cuenta;
+import org.test.app.servicespring.util.Datos;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CuentaServiceImplTest {
+    @MockBean
+    ICuentaRepository cuentaRepository;
+    @MockBean
+    IBancoRepository bancoRepository;
+    @Autowired
+    ICuentaService service;
+
+    @Test
+    void contextLoads() {
+        Mockito.when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
+        Mockito.when(cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
+        Mockito.when(bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+
+        BigDecimal saldoOrigin = service.reviewSaldo(1L);
+        BigDecimal saldoFinal = service.reviewSaldo(2L);
+
+        assertEquals("1000", saldoOrigin.toPlainString());
+        assertEquals("2000", saldoFinal.toPlainString());
+
+        service.transfer(1L, 2L, new BigDecimal("100"), 1L);
+
+        saldoOrigin = service.reviewSaldo(1L);
+        saldoFinal = service.reviewSaldo(2L);
+
+        assertEquals("900", saldoOrigin.toPlainString());
+        assertEquals("2100", saldoFinal.toPlainString());
+        assertEquals(1, service.getTotalTransferences(1L));
+
+        Mockito.verify(cuentaRepository, Mockito.times(3)).findById(1L);
+        Mockito.verify(cuentaRepository, Mockito.times(3)).findById(2L);
+        Mockito.verify(cuentaRepository, Mockito.times(2)).save(Mockito.any(Cuenta.class));
+
+        Mockito.verify(bancoRepository, Mockito.times(2)).findById(1L);
+        Mockito.verify(bancoRepository).save(Mockito.any(Banco.class));
+    }
+
+    @Test
+    void contextLoads2() {
+        Mockito.when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
+        Mockito.when(cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
+        Mockito.when(bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+
+        BigDecimal saldoOrigin = service.reviewSaldo(1L);
+        BigDecimal saldoFinal = service.reviewSaldo(2L);
+
+        assertEquals("1000", saldoOrigin.toPlainString());
+        assertEquals("2000", saldoFinal.toPlainString());
+
+        assertThrows(DineroInsuficienteException.class, () ->
+                service.transfer(1L, 2L, new BigDecimal("1200"), 1L));
+
+        saldoOrigin = service.reviewSaldo(1L);
+        saldoFinal = service.reviewSaldo(2L);
+
+        assertEquals("1000", saldoOrigin.toPlainString());
+        assertEquals("2000", saldoFinal.toPlainString());
+        assertEquals(0, service.getTotalTransferences(1L));
+
+        Mockito.verify(cuentaRepository, Mockito.times(3)).findById(1L);
+        Mockito.verify(cuentaRepository, Mockito.times(2)).findById(2L);
+        Mockito.verify(cuentaRepository, Mockito.never()).save(Mockito.any(Cuenta.class));
+
+        Mockito.verify(bancoRepository, Mockito.times(1)).findById(1L);
+        Mockito.verify(bancoRepository, Mockito.never()).save(Mockito.any(Banco.class));
+    }
+
+    @Test
+    void contextLoads3() {
+        Mockito.when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
+
+        Cuenta c1 = service.findById(1L);
+        Cuenta c2 = service.findById(1L);
+
+        assertSame(c1, c2);
+        assertEquals("Andres", c1.getPersona());
+        assertEquals("Andres", c2.getPersona());
+
+        Mockito.verify(cuentaRepository, Mockito.times(2)).findById(1L);
+    }
+
+}
