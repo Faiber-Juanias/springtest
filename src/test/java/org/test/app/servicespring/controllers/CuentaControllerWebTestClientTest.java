dab62c44ba7605251f1eaef55330b261dee7378c
fjuanias <faiberjuanias001@gmail.com>
2023-03-20 22:42:57 -0500

    Added controller with POST integration test

---
+++ src/test/java/org/test/app/servicespring/controllers/CuentaControllerWebTestClientTest.java
@@ -0,0 +1,47 @@
+package org.test.app.servicespring.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.test.app.servicespring.models.TransaccionDto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CuentaControllerWebTestClientTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testTrasferir() {
+        TransaccionDto dto = new TransaccionDto();
+        dto.setCuentaOrigenId(1L);
+        dto.setCuentaDestinoId(2L);
+        dto.setBancoId(1L);
+        dto.setMonto(new BigDecimal("100"));
+
+        client.post().uri("http://localhost:8080/api/cuentas/transferir")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.mensaje").isNotEmpty()
+                .jsonPath("$.mensaje").value(Matchers.is("Transferencia realizada con éxito"))
+                .jsonPath("$.mensaje").value(valor -> assertEquals("Transferencia realizada con éxito", valor))
+                .jsonPath("$.mensaje").isEqualTo("Transferencia realizada con éxito")
+                .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(dto.getCuentaOrigenId())
+                .jsonPath("$.date").isEqualTo(LocalDate.now().toString());
+    }
+}
